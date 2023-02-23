4dd48111da62fd474e31a99cc4b3f576ed2902c4
erdaboss99 <60655139+erdaboss99@users.noreply.github.com>
2023-02-23 16:23:53 +0100

    Implement  forwarding feature after submission

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,11 +1,17 @@
+import { useHistory } from 'react-router-dom';
+
 import NewMeetupForm from '../components/meetups/NewMeetupForm';
 
 function NewMeetupPage() {
+	const history = useHistory();
+
 	function addMeetupHandler(meetupData) {
 		fetch(process.env.REACT_APP_COURSE_NEW_MEETUP_API, {
 			method: 'POST',
 			body: JSON.stringify(meetupData),
 			headers: { 'Content-Type': 'application/json' },
+		}).then(() => {
+			history.replace('/');
 		});
 	}
 
