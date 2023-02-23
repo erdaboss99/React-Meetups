c53b659c8498f209704dc0f81d54ac03086b1c1d
erdaboss99 <60655139+erdaboss99@users.noreply.github.com>
2023-02-23 15:59:53 +0100

    Implement post request on submission

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,18 @@
 import NewMeetupForm from '../components/meetups/NewMeetupForm';
 
 function NewMeetupPage() {
+	function addMeetupHandler(meetupData) {
+		fetch(process.env.REACT_APP_COURSE_NEW_MEETUP_API, {
+			method: 'POST',
+			body: JSON.stringify(meetupData),
+			headers: { 'Content-Type': 'application/json' },
+		});
+	}
+
 	return (
 		<section>
 			<h1>Add New Meetup</h1>
-			<NewMeetupForm />
+			<NewMeetupForm onAddMeetup={addMeetupHandler} />
 		</section>
 	);
 }
